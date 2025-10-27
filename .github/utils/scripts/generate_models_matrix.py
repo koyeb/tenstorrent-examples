5329350f352d89ac70b428215ac6b541191d0a40
Edouard Bonlieu <ebonlieu@gmail.com>
2025-10-27 14:32:18 +0100

    switch from tt-inference-server dev to main

--- .github/utils/scripts/generate_models_matrix.py
+++ .github/utils/scripts/generate_models_matrix.py
@@ -11,15 +11,17 @@ for _, model_spec in MODEL_SPECS.items():
         and (
             model_spec.status == ModelStatusTypes.FUNCTIONAL
             or model_spec.status == ModelStatusTypes.EXPERIMENTAL
+            or model_spec.status == ModelStatusTypes.COMPLETE
+            or model_spec.status == ModelStatusTypes.TOP_PERF
         )
     ):
         model_dict = model_spec.get_serialized_dict()
 
         # Replace docker image tag from 'release' to 'dev'
-        if "docker_image" in model_dict:
-            model_dict["docker_image"] = model_dict["docker_image"].replace(
-                "vllm-tt-metal-src-release", "vllm-tt-metal-src-dev"
-            )
+        # if "docker_image" in model_dict:
+        #     model_dict["docker_image"] = model_dict["docker_image"].replace(
+        #         "vllm-tt-metal-src-release", "vllm-tt-metal-src-dev"
+        #     )
 
         matrix["include"].append(model_dict)
 
